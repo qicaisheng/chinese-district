844144b945c7c71d0df0326ba08369c5afcd520c
qicaisheng <qicaisheng@qq.com>
2017-07-03 23:15:29 +0800

    get the city via district code.

--- src/main/java/com/qicaisheng/util/cndistrict/District.java
+++ src/main/java/com/qicaisheng/util/cndistrict/District.java
@@ -18,4 +18,14 @@ public class District {
 
         return map.get(code);
     }
+
+    public String getCity(String districtCode) {
+        if (districtCode.endsWith("00")) return null;
+
+        Map<String, String> map = AddressParser.getInstance().parse();
+        if (!map.containsKey(districtCode)) return null;
+
+        String cityCode = districtCode.substring(4,6).concat("00");
+        return map.get(cityCode);
+    }
 }
--- src/test/java/com/qicaisheng/util/cndistrict/DistrictTest.java
+++ src/test/java/com/qicaisheng/util/cndistrict/DistrictTest.java
@@ -12,4 +12,14 @@ public class DistrictTest {
         Assert.assertEquals(null, district.get("error code"));
         Assert.assertEquals("南山区", district.get("440305"));
     }
+
+    @Test
+    public void should_get_city_via_district_code() {
+        District district = District.getInstance();
+
+
+        Assert.assertEquals(null, district.getCity("440000"));
+        Assert.assertEquals(null, district.getCity("error code"));
+        Assert.assertEquals("深圳市", district.getCity("440305"));
+    }
 }
